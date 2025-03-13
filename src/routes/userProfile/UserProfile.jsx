c01eecca9230e2968b804deb3fd822e0cce862bd
Sanjar Kamaliddinov <sanjarkama26@gmail.com>
2025-03-13 15:06:04 +0500

    .

--- src/routes/userProfile/UserProfile.jsx
+++ src/routes/userProfile/UserProfile.jsx
@@ -1,4 +1,4 @@
-import "./userProfile.css"
+import "./UserProfile.css"
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react"
 import { toast } from 'react-toastify';
